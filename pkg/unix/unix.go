1907f1419de500a723146e9866ca900061489f93
Crow <5488190+echocrow@users.noreply.github.com>
2021-05-31 01:48:24 +0200

    Add basic time parser

---
+++ pkg/unix/unix.go
@@ -0,0 +1,107 @@
+// Package unix provides UNIX helper functions.
+package unix
+
+import (
+	"errors"
+	"regexp"
+	"strings"
+	"time"
+)
+
+// UNIX parse errors.
+var (
+	ErrInvalidTimeLayout = errors.New("date input format not recognized")
+)
+
+// Standard time layouts, based on Go's layout template:
+// Mon Jan 2 15:04:05 -0700 MST 2006
+var stdTimeLayouts = []string{
+	time.RFC3339Nano,
+	time.RFC3339,
+	time.StampNano,
+	time.Stamp,
+	time.StampMilli,
+	time.StampMicro,
+	time.ANSIC,
+	time.UnixDate,
+	time.RFC822,
+	time.RFC822Z,
+}
+
+// Allowed date layouts.
+var dateLayouts = []string{
+	"2006-01-02",
+	"2006/01/02",
+	"06-01-02",
+	"02-01-06",
+	"02.01.2006",
+	"02.01.06",
+	"02/01/06",
+	"01/02/06",
+	"Jan 02 2006",
+	"02 Jan 2006",
+}
+
+// Allowed time layouts
+var timeLayout = []string{
+	"15:04:05",
+	"15:04",
+	"",
+}
+
+// Parse auto-detects the date/time format of a string and parses it to the
+// time value it represents.
+func Parse(
+	s string,
+) (t time.Time, layout string, err error) {
+	for _, layout = range stdTimeLayouts {
+		if t, err = time.Parse(layout, s); err == nil {
+			return
+		}
+	}
+
+	for _, dateL := range dateLayouts {
+		for _, timeL := range timeLayout {
+			if t, layout, err = parseDateTime(dateL, timeL, s); err == nil {
+				return
+			}
+		}
+	}
+
+	ss := shortenStr(s)
+	for _, dateL := range dateLayouts {
+		dateL = shortenStr(dateL)
+		for _, timeL := range timeLayout {
+			if t, layout, err = parseDateTime(dateL, timeL, ss); err == nil {
+				return
+			}
+		}
+	}
+
+	return time.Time{}, "", ErrInvalidTimeLayout
+}
+
+func shortenStr(s string) string {
+	s = regexp.MustCompile(`\s{2,}`).ReplaceAllString(s, "")
+	s = regexp.MustCompile(`\s(\W)`).ReplaceAllString(s, "$1")
+	s = regexp.MustCompile(`(\W)\s`).ReplaceAllString(s, "$1")
+	return s
+}
+
+func parseDateTime(
+	dateL string,
+	timeL string,
+	input string,
+) (t time.Time, layout string, err error) {
+	layout = strings.Trim(dateL+" "+timeL, " ")
+	if t, err = time.Parse(layout, input); err == nil {
+		return
+	}
+
+	layout = strings.Trim(timeL+" "+dateL, " ")
+	if t, err = time.Parse(layout, input); err == nil {
+		return
+	}
+
+	return time.Time{}, "", ErrInvalidTimeLayout
+}
---
+++ pkg/unix/unix_test.go
@@ -0,0 +1,59 @@
+package unix_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/echocrow/unix/pkg/unix"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseString(t *testing.T) {
+	tests := []struct {
+		s      string
+		wantTs int64
+		wantF  string
+	}{
+		{"2020-01-02", 1577923200, "2006-01-02"},
+		{"2020-01-02 00:00:00", 1577923200, "2006-01-02 15:04:05"},
+		{"2020-01-02T00:00:00Z", 1577923200, time.RFC3339Nano},
+		{"Jan 02 2020", 1577923200, "Jan 02 2006"},
+		{"02 Jan 2020", 1577923200, "02 Jan 2006"},
+		{"20-01-02", 1577923200, "06-01-02"},
+
+		{"99-12-31", 946598400, "06-01-02"},
+		{"31-12-99", 946598400, "02-01-06"},
+		{"31.12.99", 946598400, "02.01.06"},
+		{"31.12. 1999", 946598400, "02.01.2006"},
+		{"31.12.1999", 946598400, "02.01.2006"},
+
+		{"1999-12-31 23:59", 946684740, "2006-01-02 15:04"},
+		{"99-12-31 23:59", 946684740, "06-01-02 15:04"},
+
+		{"1999-12-31 23:59:58", 946684798, "2006-01-02 15:04:05"},
+		{"99-12-31 23:59:58", 946684798, "06-01-02 15:04:05"},
+		{"31. 12. 1999 23:59:58", 946684798, "02.01.2006 15:04:05"},
+		{"31. 12. 99 23:59:58", 946684798, "02.01.06 15:04:05"},
+
+		{"invalid input", -1, ""},
+		{"99-99-99", -1, ""},
+	}
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.s, func(t *testing.T) {
+			t.Parallel()
+			got, gotF, err := unix.Parse(tc.s)
+			wantTs := tc.wantTs
+			if wantTs >= 0 {
+				want := time.Unix(wantTs, 0).In(time.UTC)
+				assert.Equal(t, want, got)
+				assert.Equal(t, tc.wantF, gotF)
+				assert.NoError(t, err)
+			} else {
+				want := time.Time{}
+				assert.Equal(t, want, got)
+				assert.Error(t, err)
+			}
+		})
+	}
+}
